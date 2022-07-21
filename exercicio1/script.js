efa49d505d2d4be00963f557fc1445a853b843ab
thiagogagliari <thiagogagliari@hotmail.com>
2022-07-21 15:49:32 -0300

    Resoluçao Aula Operadores 008

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let numPar = Number(prompt("Insira um número par?"))
+console.log("O resto da divisão é", numPar % 2 )
+
+//Resposta da C: O resto de qualquer número par é 0 aritmeticamente.
+//Resposta da D: Qualquer número ímpar digitado terá seu resto diferente de zero.
